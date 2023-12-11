61cf8508c1e51854bbc2546f8f914645d8338555
bbwark <devivobruno01@gmail.com>
2023-12-11 03:34:18 +0100

    Update of User and Credit Card
    Attributes added to classes.
    Parameters email and password added to User

--- app/src/main/java/com/CioffiDeVivo/dietideals/DataModels/User.kt
+++ app/src/main/java/com/CioffiDeVivo/dietideals/DataModels/User.kt
@@ -2,12 +2,22 @@ package com.CioffiDeVivo.dietideals.DataModels
 
 class User(
     name: String,
+    email: String,
+    password: String,
     isSeller: Boolean = false,
     bio: String = "",
     webLinks: String = "",
     address: String = "",
     phoneNumber: String = "",
-    creditCard: Array<CreditCard> = arrayOf()
+    creditCards: Array<CreditCard> = arrayOf()
 ){
-
+    var name = name
+    var email = email
+    var password = password
+    var isSeller = isSeller
+    var bio = bio
+    var webLinks = webLinks
+    var address = address
+    var phoneNumber = phoneNumber
+    var creditCards = creditCards
 }
