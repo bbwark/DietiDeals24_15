acbb8513f3b07b06cfc151743de45affb013a6c4
bbwark <devivobruno01@gmail.com>
2023-12-12 18:08:12 +0100

    Auction Attributes Fix
    ownerId added

--- app/src/main/java/com/CioffiDeVivo/dietideals/DataModels/Auction.kt
+++ app/src/main/java/com/CioffiDeVivo/dietideals/DataModels/Auction.kt
@@ -5,11 +5,13 @@ import java.util.UUID
 
 class Auction(
     id: UUID,
+    ownerId: UUID,
     bids: Array<Bid> = arrayOf(),
     endingDate: LocalDate,
     auctionType: AuctionType
 ){
     val id: UUID = id
+    val ownerId: UUID = ownerId
     var bids: Array<Bid> = bids
     var endingDate: LocalDate = endingDate
     val auctionType: AuctionType = auctionType
