3a17a303dfdeb0ddbfc814d65ffbe6ccc3d90c97
Seyteru <s.cioffi2001@gmail.com>
2023-12-13 18:52:53 +0100

    Creation of: HomeView, RegisterView, LogInView. (No navigation yet)

---
+++ app/src/main/java/com/CioffiDeVivo/dietideals/ui/HomeView.kt
@@ -0,0 +1,75 @@
+package com.CioffiDeVivo.dietideals.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Favorite
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HomeView(){
+    Column(horizontalAlignment = Alignment.End,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Spacer(modifier = Modifier.height(18.dp))
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(Icons.Rounded.Favorite, contentDescription = null)
+        }
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        Spacer(
+            modifier = Modifier.height(40.dp))
+        Text(
+            "DietiDeals",
+            fontSize = 30.sp,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Bold,
+            color = Color.DarkGray
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+        ElevatedButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.size(width = 330.dp, height = 50.dp),
+            content = {
+
+                Icon(Icons.Rounded.Search, contentDescription = null)
+                Spacer(modifier = Modifier.width(10.dp))
+                Text(
+                    "Search on DietiDeals",
+                    fontSize = 15.sp,
+                    color = Color.Gray,
+                )
+                Spacer(modifier = Modifier.width(110.dp))
+            }
+        )
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeViewPreview(){
+    HomeView()
+}
---
+++ app/src/main/java/com/CioffiDeVivo/dietideals/ui/RegisterView.kt
@@ -0,0 +1,142 @@
+package com.CioffiDeVivo.dietideals.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.CioffiDeVivo.dietideals.R
+
+@Composable
+fun Register() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Spacer(
+            modifier = Modifier.height(40.dp))
+        Text(
+            "Create an Account",
+            fontSize = 30.sp,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Bold,
+            color = Color.DarkGray
+        )
+        Spacer(
+            modifier = Modifier.height(450.dp)
+        )
+
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.size(width = 330.dp, height = 50.dp),
+            content = {
+                Text(stringResource(R.string.ContinuewithEmail), fontSize = 20.sp)
+            }
+        )
+        Spacer(
+            modifier = Modifier.height(5.dp)
+        )
+        Text(
+            "or", fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.Medium
+        )
+        Spacer(
+            modifier = Modifier.height(5.dp)
+        )
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.size(width = 330.dp, height = 50.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.White,
+                contentColor = Color.Black
+            ),
+            content = {
+                Image(
+                    painter = painterResource(id = R.drawable.logogoogle),
+                    contentDescription = null,
+                    modifier = Modifier.size(25.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    stringResource(R.string.ContinuewithGoogle),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+        )
+        Spacer(
+            modifier = Modifier.height(5.dp)
+        )
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.size(width = 330.dp, height = 50.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.White,
+                contentColor = Color.Black
+            ),
+            content = {
+                Image(
+                    painter = painterResource(id = R.drawable.logogit),
+                    contentDescription = null,
+                    modifier = Modifier.size(25.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+
+                    stringResource(R.string.ContinuewithGit),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+            }
+        )
+        Spacer(
+            modifier = Modifier.height(5.dp)
+        )
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.size(width = 330.dp, height = 50.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.White,
+                contentColor = Color.Black
+            ),
+            content = {
+                Image(
+                    painter = painterResource(id = R.drawable.logofacebook),
+                    contentDescription = null,
+                    modifier = Modifier.size(25.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    stringResource(R.string.ContinuewithFacebook),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+            }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RegisterPreview(){
+    Register()
+}
