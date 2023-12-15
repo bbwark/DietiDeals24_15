d836bd88511acdbb6819521df0307cc6f0dd7b8e
bbwark <devivobruno01@gmail.com>
2023-12-15 17:49:26 +0100

    Refactor Theme Name

--- app/src/main/java/com/CioffiDeVivo/dietideals/ui/theme/Theme.kt
+++ app/src/main/java/com/CioffiDeVivo/dietideals/ui/theme/Theme.kt
@@ -73,7 +73,7 @@ private val DarkColors = darkColorScheme(
 )
 
 @Composable
-fun AppTheme(
+fun DietiDealsTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
