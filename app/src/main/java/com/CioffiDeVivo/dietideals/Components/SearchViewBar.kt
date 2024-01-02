15ed61473e8439d6aa5bab4169d62f008bbcfdf0
bbwark <96076911+bbwark@users.noreply.github.com>
2024-01-03 00:49:31 +0100

    SearchBar Component Creation

---
+++ app/src/main/java/com/CioffiDeVivo/dietideals/Components/SearchViewBar.kt
@@ -0,0 +1,78 @@
+package com.CioffiDeVivo.dietideals.Components
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchViewBar(
+    modifier: Modifier = Modifier,
+    state: MutableState<TextFieldValue>
+) {
+
+    TextField(
+        value = state.value,
+        onValueChange = { value ->
+            state.value = value
+            //add API Request here
+            //it is possible to use a debounce modifier to delay the request of a fixed amount of time to optimize the number of the requests
+        },
+        modifier = modifier.fillMaxWidth(),
+        textStyle = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.onPrimary),
+        leadingIcon = {
+            Icon(
+                Icons.Default.Search,
+                contentDescription = "",
+                modifier = Modifier
+                    .padding(15.dp)
+                    .size(24.dp)
+            )
+        },
+        trailingIcon = {
+            if (state.value != TextFieldValue("")) {
+                IconButton(
+                    onClick = {
+                        state.value =
+                            TextFieldValue("") // Remove text from TextField when you press the 'X' icon
+                    }
+                ) {
+                    Icon(
+                        Icons.Default.Close,
+                        contentDescription = "",
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .size(24.dp)
+                    )
+                }
+            }
+        },
+        singleLine = true,
+        shape = RectangleShape
+    )
+}
+
+@SuppressLint("UnrememberedMutableState")
+@Preview
+@Composable
+fun SearchViewBarPreview() {
+    var text: MutableState<TextFieldValue> = mutableStateOf(TextFieldValue("test"))
+    SearchViewBar(state = text)
+}
