371bac092a21064b2ef24f59cb41bfceba0f375b
bbwark <devivobruno01@gmail.com>
2024-01-13 10:27:58 +0100

    AccountViewButton Created

--- app/src/main/java/com/CioffiDeVivo/dietideals/Views/AccountView.kt
+++ app/src/main/java/com/CioffiDeVivo/dietideals/Views/AccountView.kt
@@ -1,38 +1,47 @@
-package com.CioffiDeVivo.dietideals.Views.Navigation
+package com.CioffiDeVivo.dietideals.Views
 
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ChevronRight
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.CioffiDeVivo.dietideals.DietiDealsViewModel
-import com.CioffiDeVivo.dietideals.ui.theme.md_theme_light_secondary
 import com.CioffiDeVivo.dietideals.ui.theme.md_theme_light_secondaryContainer
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun AccountView(viewModel: DietiDealsViewModel, navController: NavHostController) {
-    Column {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
         AccountViewTopBar(viewModel.user.name, viewModel.user.email)
+        Spacer(modifier = Modifier.size(24.dp))
     }
 }
 
@@ -59,6 +68,29 @@ fun AccountViewTopBar(userName: String, userEmail: String) {
     }
 }
 
+@Composable
+fun AccountViewButton(navController: NavHostController, destinationRoute: String, caption: String, icon: ImageVector) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 12.dp)
+            .clickable { /* navigate to destination route view */ }
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            modifier = Modifier.size(24.dp)
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        Text(text = caption, fontSize = 18.sp, fontWeight = FontWeight(600))
+        Spacer(modifier = Modifier.weight(1f))
+        Icon(
+            imageVector = Icons.Default.ChevronRight,
+            contentDescription = null
+        )
+    }
+}
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Preview (showBackground = true)
