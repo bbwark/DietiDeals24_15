60869af907b18ffd321d3bd2d54ddd5a59b07ff9
bbwark <devivobruno01@gmail.com>
2024-01-13 14:40:20 +0100

    ManageCards Completed and minor changes
    ViewModel has 3 new credit cards only for testing purposes.
    CreditCard data model has now an expirationDate.
    Reformatting of texts

--- app/src/main/java/com/CioffiDeVivo/dietideals/DataModels/CreditCard.kt
+++ app/src/main/java/com/CioffiDeVivo/dietideals/DataModels/CreditCard.kt
@@ -1,9 +1,13 @@
 package com.CioffiDeVivo.dietideals.DataModels
 
+import java.time.LocalDate
+
 class CreditCard(
     number: String,
-    cvv: String
+    cvv: String,
+    expirationDate: LocalDate
 ) {
     val number: String = number
     val cvv: String = cvv
+    val expirationDate: LocalDate = expirationDate
 }
--- app/src/main/java/com/CioffiDeVivo/dietideals/Views/ManageCardsView.kt
+++ app/src/main/java/com/CioffiDeVivo/dietideals/Views/ManageCardsView.kt
@@ -1,15 +1,45 @@
 package com.CioffiDeVivo.dietideals.Views
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import com.CioffiDeVivo.dietideals.Components.ManageCardsElement
+import com.CioffiDeVivo.dietideals.DietiDealsViewModel
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun ManageCardsView() {
-
+fun ManageCardsView(viewModel: DietiDealsViewModel) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 12.dp),
+        content = {
+            itemsIndexed(viewModel.user.creditCards) { index, item ->
+                if (index == 0) {
+                    Spacer(modifier = Modifier.height(10.dp))
+                }
+                ManageCardsElement(
+                    cardNumber = item.number,
+                    clickOnMore = {/* a menu where you can delete the card has to open */ })
+                Divider()
+            }
+        })
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview (showBackground = true)
 @Composable
 fun ManageCardsViewPreview() {
-
+    ManageCardsView(viewModel = DietiDealsViewModel())
 }
