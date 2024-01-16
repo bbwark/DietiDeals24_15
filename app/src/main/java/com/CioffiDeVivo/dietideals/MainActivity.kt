40f7019b79e26054b1b222e373b323427f61f4b7
bbwark <devivobruno01@gmail.com>
2024-01-16 16:08:34 +0100

    MainActivity Conflicts Fix

--- app/src/main/java/com/CioffiDeVivo/dietideals/MainActivity.kt
+++ app/src/main/java/com/CioffiDeVivo/dietideals/MainActivity.kt
@@ -5,7 +5,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import com.CioffiDeVivo.dietideals.Views.RegisterCredentialsView
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.CioffiDeVivo.dietideals.Views.Navigation.SetupNavGraph
 import com.CioffiDeVivo.dietideals.ui.theme.DietiDealsTheme
 
 
@@ -16,9 +21,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DietiDealsTheme {
-                RegisterCredentialsView(viewModel = DietiDealsViewModel())
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    var dietiDealsViewModel = DietiDealsViewModel()
+                    SetupNavGraph(navController = navController, viewModel = dietiDealsViewModel)
+                }
             }
         }
     }
 }
-
