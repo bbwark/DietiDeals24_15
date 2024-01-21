6973c464341311d3eaf4081d6a9cb78c6b784642
bbwark <devivobruno01@gmail.com>
2024-01-21 03:06:01 +0100

    BidHistoryElement Creation

--- app/src/main/java/com/CioffiDeVivo/dietideals/Components/BidHistoryElement.kt
+++ app/src/main/java/com/CioffiDeVivo/dietideals/Components/BidHistoryElement.kt
@@ -3,12 +3,10 @@ package com.CioffiDeVivo.dietideals.Components
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CreditCard
-import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.MoreHoriz
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -22,39 +20,28 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.CioffiDeVivo.dietideals.R
 
 @Composable
-fun ManageCardsElement(modifier: Modifier = Modifier, cardNumber: String, clickOnDelete: () -> Unit) {
+fun BidHistoryElement(
+    bidderName: String,
+    onShowDetails: () -> Unit,
+    onAcceptOffer: () -> Unit,
+    onUserInfo: () -> Unit
+) {
     var expanded by remember { mutableStateOf(false) }
-    val circuit = CardCircuit(cardNumber)
 
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier.fillMaxWidth()
     ) {
-        Box(Modifier.size(48.dp)) {
-            if (circuit != -1) {
-                Icon(
-                    painter = painterResource(id = circuit),
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxSize(),
-                    tint = Color.Unspecified
-                )
-            } else {
-                Icon(
-                    imageVector = Icons.Default.CreditCard,
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxSize()
-                )
-            }
-        }
+        Icon(
+            imageVector = Icons.Default.AccountCircle,
+            contentDescription = null
+        )
         Spacer(modifier = Modifier.size(8.dp))
-        Text(text = "•".repeat(cardNumber.length - 4).plus(cardNumber.takeLast(4)))
+        Text(text = bidderName)
         Spacer(modifier = Modifier.weight(1f))
         Box {
             IconButton(onClick = { expanded = true }) {
@@ -67,28 +54,16 @@ fun ManageCardsElement(modifier: Modifier = Modifier, cardNumber: String, clickO
                 expanded = expanded,
                 onDismissRequest = { expanded = false }
             ) {
-                DropdownMenuItem(
-                    text = { Text(text = "Delete") },
-                    onClick = { clickOnDelete() },
-                    leadingIcon = { Icon(Icons.Default.Delete, contentDescription = null) }
-                )
+                DropdownMenuItem(text = { Text(text = "Show Details") }, onClick = { onShowDetails() })
+                DropdownMenuItem(text = { Text(text = "Accept This Offer") }, onClick = { onAcceptOffer() })
+                DropdownMenuItem(text = { Text(text = "User Ifno") }, onClick = { onUserInfo() })
             }
         }
     }
 }
 
-fun CardCircuit(cardNumber: String): Int {
-    if (cardNumber.first() == '4') {
-        return R.drawable.visa
-    }
-    if (cardNumber.first() == '5') {
-        return R.drawable.mastercard
-    }
-    return -1
-}
-
 @Preview(showBackground = true)
 @Composable
-fun ManageCardsElementPreview() {
-    ManageCardsElement(cardNumber = "5234567891234567", clickOnDelete = {})
+fun BidHistoryElementPreview() {
+
 }
---
+++ app/src/main/java/com/CioffiDeVivo/dietideals/Views/BidHistoryView.kt
@@ -0,0 +1,19 @@
+package com.CioffiDeVivo.dietideals.Views
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun BidHistoryView() {
+    LazyColumn(content = {
+
+
+    })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BidHistoryViewPreview() {
+
+}
