3442b9f2192dbb0eedc536becda68184860b4ca5
bbwark <devivobruno01@gmail.com>
2024-02-19 17:50:58 +0100

    Create AuctionCategory.kt

---
+++ front/app/src/main/java/com/CioffiDeVivo/dietideals/DataModels/AuctionCategory.kt
@@ -0,0 +1,12 @@
+package com.CioffiDeVivo.dietideals.DataModels;
+
+enum class AuctionCategory {
+    Elettronica,
+    Giochi,
+    Casa,
+    Motori,
+    Libri,
+    Moda,
+    Sport,
+    Musica
+}
