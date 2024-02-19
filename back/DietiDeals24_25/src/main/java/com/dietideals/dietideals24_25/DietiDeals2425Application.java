49c0b7b96ae3db4c4c9c7f63eacd9129a6e4a7e6
bbwark <devivobruno01@gmail.com>
2024-02-19 17:36:51 +0100

    SpringBoot Initialization

---
+++ back/DietiDeals24_25/src/main/java/com/dietideals/dietideals24_25/DietiDeals2425Application.java
@@ -0,0 +1,13 @@
+package com.dietideals.dietideals24_25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietiDeals2425Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietiDeals2425Application.class, args);
+    }
+
+}
