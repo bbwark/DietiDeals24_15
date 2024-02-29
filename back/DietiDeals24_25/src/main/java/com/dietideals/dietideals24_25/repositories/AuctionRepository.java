8a7ba3ae8f1d9132fb594ec79d8edc6e36981d06
bbwark <devivobruno01@gmail.com>
2024-02-29 19:42:23 +0100

    randomAuctions and only auctions name problem solved

--- back/DietiDeals24_25/src/main/java/com/dietideals/dietideals24_25/repositories/AuctionRepository.java
+++ back/DietiDeals24_25/src/main/java/com/dietideals/dietideals24_25/repositories/AuctionRepository.java
@@ -1,6 +1,7 @@
 package com.dietideals.dietideals24_25.repositories;
 
 import com.dietideals.dietideals24_25.domain.entities.AuctionEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -13,5 +14,8 @@ import java.util.UUID;
 public interface AuctionRepository extends CrudRepository<AuctionEntity, UUID> {
     @Query("SELECT a FROM auctions a WHERE lower(a.item.name) LIKE lower(concat('%', :itemName,'%'))") //TODO: check if works
     List<AuctionEntity> findByItemName(@Param("itemName") String itemName);
+
+    @Query("SELECT a FROM Auction a WHERE a.owner.uuid != :ownerUuid")
+    List<AuctionEntity> findRandomAuctions(@Param("ownerUuid") UUID ownerUuid, Pageable pageable);
 
 }
