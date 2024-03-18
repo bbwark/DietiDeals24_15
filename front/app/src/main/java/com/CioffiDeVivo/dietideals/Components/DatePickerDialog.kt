e1bf1f2b342ba3c67c663fcaacdf939606d9b469
Seyteru <s.cioffi2001@gmail.com>
2024-03-18 21:13:54 +0100

    Final changes
    Final changes on the other composable.

--- front/app/src/main/java/com/CioffiDeVivo/dietideals/Components/DatePickerDialog.kt
+++ front/app/src/main/java/com/CioffiDeVivo/dietideals/Components/DatePickerDialog.kt
@@ -1,24 +1,16 @@
 package com.CioffiDeVivo.dietideals.Components
 
-import android.os.Build
-import androidx.annotation.RequiresApi
-import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SelectableDates
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
-import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import java.util.Date
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyDatePickerDialog(
+fun CustomDatePickerDialog(
     onAccept: (Long?) -> Unit,
     onCancel: () -> Unit
 ) {
