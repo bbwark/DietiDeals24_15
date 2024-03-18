0ad6c8f7d7d302e9b350dffb396496e4143b50ea
Seyteru <s.cioffi2001@gmail.com>
2024-03-18 21:02:13 +0100

    DataPicker
    Implemented correctly datapicker with mvvm flow.

--- front/app/src/main/java/com/CioffiDeVivo/dietideals/Components/DatePickerDialogs.kt
+++ front/app/src/main/java/com/CioffiDeVivo/dietideals/Components/DatePickerDialogs.kt
@@ -1,59 +1,42 @@
 package com.CioffiDeVivo.dietideals.Components
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SelectableDates
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.Date
-
-private fun convertMillisToDate(millis: Long): String {
-    val formatter = SimpleDateFormat("dd/MM/yyyy")
-    return formatter.format(Date(millis))
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyDatePickerDialog(
-    onDateSelected: (String) -> Unit,
-    onDismiss: () -> Unit
+    onAccept: (Long?) -> Unit,
+    onCancel: () -> Unit
 ) {
-    val datePickerState = rememberDatePickerState(selectableDates = object : SelectableDates {
-        override fun isSelectableDate(utcTimeMillis: Long): Boolean {
-            return utcTimeMillis <= System.currentTimeMillis()
-        }
-    })
-
-    val selectedDate = datePickerState.selectedDateMillis?.let {
-        convertMillisToDate(it)
-    } ?: ""
+    val state = rememberDatePickerState()
 
     DatePickerDialog(
-        onDismissRequest = { onDismiss() },
+        onDismissRequest = {  },
         confirmButton = {
-            Button(onClick = {
-                onDateSelected(selectedDate)
-                onDismiss()
-            }
-
-            ) {
-                Text(text = "OK")
+            TextButton(onClick = { onAccept(state.selectedDateMillis) }) {
+                Text("Confirm")
             }
         },
         dismissButton = {
-            Button(onClick = {
-                onDismiss()
-            }) {
-                Text(text = "Cancel")
+            TextButton(onClick = onCancel) {
+                Text("Cancel")
             }
         }
     ) {
-        DatePicker(
-            state = datePickerState
-        )
+        DatePicker(state = state)
     }
 }
