33994a2e1430ef17a3a1c15e771d79c609748964
bbwark <devivobruno01@gmail.com>
2024-08-05 03:18:30 +0200

    Bid by auctionId implementation and added to controller

--- back/DietiDeals24_25/src/main/java/com/dietideals/dietideals24_25/repositories/BidRepository.java
+++ back/DietiDeals24_25/src/main/java/com/dietideals/dietideals24_25/repositories/BidRepository.java
@@ -2,11 +2,17 @@ package com.dietideals.dietideals24_25.repositories;
 
 import com.dietideals.dietideals24_25.domain.entities.BidEntity;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface BidRepository extends CrudRepository<BidEntity, UUID> {
+
+    @Query("SELECT b FROM BidEntity b WHERE b.auction.id = :auctionId ORDER BY b.value DESC")
+    List<BidEntity> findByAuctionId(@Param("auctionId") UUID auctionId);
 }
