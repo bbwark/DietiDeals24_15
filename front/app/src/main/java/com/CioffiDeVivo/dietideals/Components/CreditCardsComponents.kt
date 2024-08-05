efdf868de876103a00de4c3ceaebbe2db9018374
Seyteru <s.cioffi2001@gmail.com>
2024-08-05 23:46:43 +0200

    Changed States
    Changed ViewModels with the correct states.
    Chaged CreateAuctionViewModel.
    Todo: correct the ending date with the correct way using viewmodel

--- front/app/src/main/java/com/CioffiDeVivo/dietideals/Components/CreditCardsComponents.kt
+++ front/app/src/main/java/com/CioffiDeVivo/dietideals/Components/CreditCardsComponents.kt
@@ -7,14 +7,68 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.CioffiDeVivo.dietideals.domain.DataModels.CreditCard
 import com.CioffiDeVivo.dietideals.R
 import com.CioffiDeVivo.dietideals.Views.modifierStandard
+import com.CioffiDeVivo.dietideals.viewmodel.state.AddCardState
 import com.CioffiDeVivo.dietideals.viewmodel.state.RegistrationState
 
 @Composable
-fun CreditCardFields(
+fun CreditCardFieldsOnRegisterCredentials(
     userState: RegistrationState,
+    onNumberChange: (String) -> Unit,
+    onDateChange: (String) -> Unit,
+    onCvvChange: (String) -> Unit,
+    onIbanChange: (String) -> Unit,
+    onDeleteCardNumber: (String) -> Unit,
+    onDeleteExpirationDate: (String) -> Unit,
+    onDeleteCvv: (String) -> Unit,
+    onDeleteIban: (String) -> Unit,
+){
+    InputTextField(
+        value = userState.creditCardNumber,
+        onValueChanged = { onNumberChange(it) },
+        label = stringResource(R.string.creditcard),
+        isError = userState.creditCardNumberErrorMsg != null,
+        supportingText = userState.creditCardNumberErrorMsg,
+        onTrailingIconClick = { onDeleteCardNumber(it) },
+        modifier = modifierStandard
+    )
+    Row {
+        InputTextField(
+            value = userState.expirationDate,
+            onValueChanged = { onDateChange(it) },
+            label = stringResource(R.string.expirationdate),
+            isError = userState.expirationDateErrorMsg != null,
+            placeholder = "MM/YY",
+            supportingText = userState.expirationDateErrorMsg,
+            onTrailingIconClick = { onDeleteExpirationDate(it) },
+            modifier = Modifier.width(145.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        InputTextField(
+            value = userState.cvv,
+            onValueChanged = { onCvvChange(it) },
+            label = stringResource(R.string.cvv),
+            isError = userState.cvvErrorMsg != null,
+            supportingText = userState.cvvErrorMsg,
+            onTrailingIconClick = { onDeleteCvv(it) },
+            modifier = Modifier.width(145.dp)
+        )
+    }
+    InputTextField(
+        value = userState.iban,
+        onValueChanged = { onIbanChange(it) },
+        label = stringResource(R.string.iban),
+        isError = userState.ibanErrorMsg != null,
+        supportingText = userState.ibanErrorMsg,
+        onTrailingIconClick = { onDeleteIban(it) },
+        modifier = modifierStandard
+    )
+}
+
+@Composable
+fun CreditCardFieldsOnAddCard(
+    userState: AddCardState,
     onNumberChange: (String) -> Unit,
     onDateChange: (String) -> Unit,
     onCvvChange: (String) -> Unit,
