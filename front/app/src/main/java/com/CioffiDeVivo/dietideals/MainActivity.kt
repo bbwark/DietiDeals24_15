66fefa14561342d9fe4540419d9c1c34c6447492
bbwark <devivobruno01@gmail.com>
2024-08-11 16:44:59 +0200

    Main Activity initializing singleton utilities
    Primitive automatic login added to mainactivity (to test)

--- front/app/src/main/java/com/CioffiDeVivo/dietideals/MainActivity.kt
+++ front/app/src/main/java/com/CioffiDeVivo/dietideals/MainActivity.kt
@@ -1,38 +1,66 @@
 package com.CioffiDeVivo.dietideals
 
-import android.os.Build
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
-import com.CioffiDeVivo.dietideals.Components.GoogleButton
 import com.CioffiDeVivo.dietideals.Views.Navigation.SetupNavGraph
 import com.CioffiDeVivo.dietideals.ui.theme.DietiDealsTheme
+import com.CioffiDeVivo.dietideals.utils.ApiService
+import com.CioffiDeVivo.dietideals.utils.AuthService
+import com.CioffiDeVivo.dietideals.utils.EncryptedPreferencesManager
 import com.CioffiDeVivo.dietideals.viewmodel.MainViewModel
-import com.CioffiDeVivo.dietideals.viewmodel.RegisterCredentialsViewModel
-
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import io.ktor.client.statement.bodyAsText
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        EncryptedPreferencesManager.initialize(this)
+        AuthService.initialize(this)
+
+        val encryptedSharedPreferences = EncryptedPreferencesManager.getEncryptedPreferences()
+
+        // ONLY FOR TESTING PURPOSE WE WIPE OUT PREFERENCES EACH TIME
+        val sharedPreferences = getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+        encryptedSharedPreferences.edit().apply {
+            clear()
+            apply()
+        }
+        sharedPreferences.edit().apply {
+            clear()
+            apply()
+        }
+
+
+        val email = encryptedSharedPreferences.getString("userId", null)
+        val password = encryptedSharedPreferences.getString("password", null)
+        if (email != null && password != null) {
+            lifecycleScope.launch {//change to runBlocking if you want to block main thread to wait for login (safer)
+                val loginResponse = AuthService.loginUser(email, password)
+                val sessionToken = Gson().fromJson(loginResponse.bodyAsText(), JsonObject::class.java).get("jwt").asString
+                ApiService.initialize(sessionToken, this@MainActivity)
+            }
+        }
         setContent {
             DietiDealsTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     val navController = rememberNavController()
-                    val dietiDealsViewModel = MainViewModel()
-                    SetupNavGraph(navController = navController, mainViewModel = MainViewModel())
-
+                    SetupNavGraph(
+                        navController = navController,
+                        mainViewModel = MainViewModel()
+                    )
                 }
             }
         }
