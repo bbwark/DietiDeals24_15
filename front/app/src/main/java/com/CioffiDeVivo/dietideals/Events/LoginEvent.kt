8a5c81eb5011d46c3ab9035882465a888104d5a5
bbwark <devivobruno01@gmail.com>
2024-08-11 04:46:03 +0200

    LoginView and ViewModel modified to make login call

--- front/app/src/main/java/com/CioffiDeVivo/dietideals/Events/LoginEvent.kt
+++ front/app/src/main/java/com/CioffiDeVivo/dietideals/Events/LoginEvent.kt
@@ -4,5 +4,5 @@ open class LoginEvent {
     data class EmailChanged(val email: String) : LoginEvent()
     data class EmailDeleted(val email: String) : LoginEvent()
     data class PasswordChanged(val password: String) : LoginEvent()
-    object Submit: LoginEvent()
+    data class Submit(val submitted: Boolean = true) : LoginEvent()
 }
--- front/app/src/main/java/com/CioffiDeVivo/dietideals/viewmodel/LogInCredentialsViewModel.kt
+++ front/app/src/main/java/com/CioffiDeVivo/dietideals/viewmodel/LogInCredentialsViewModel.kt
@@ -1,12 +1,22 @@
 package com.CioffiDeVivo.dietideals.viewmodel
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import com.CioffiDeVivo.dietideals.utils.ApiService
+import com.CioffiDeVivo.dietideals.utils.AuthService
 import com.CioffiDeVivo.dietideals.Events.LoginEvent
-import com.CioffiDeVivo.dietideals.Events.RegistrationEvent
 import com.CioffiDeVivo.dietideals.domain.use_case.ValidateLogInForm
 import com.CioffiDeVivo.dietideals.domain.use_case.ValidationState
+import com.CioffiDeVivo.dietideals.utils.EncryptedPreferencesManager
 import com.CioffiDeVivo.dietideals.viewmodel.state.LogInState
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.isSuccess
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -14,46 +24,59 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
-class LogInCredentialsViewModel( private val validateLogInForms: ValidateLogInForm = ValidateLogInForm() ) : ViewModel() {
+class LogInCredentialsViewModel(
+    application: Application,
+    private val validateLogInForms: ValidateLogInForm = ValidateLogInForm()
+) : AndroidViewModel(application) {
 
     private val _userLogInState = MutableStateFlow(LogInState())
     val userLogInState: StateFlow<LogInState> = _userLogInState.asStateFlow()
     private val validationEventChannel = Channel<ValidationState>()
     val validationLogInEvent = validationEventChannel.receiveAsFlow()
 
-    fun loginOnAction(loginEvent: LoginEvent){
-        when(loginEvent){
+    private val sharedPreferences by lazy {
+        application.getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+    }
+
+    fun loginOnAction(loginEvent: LoginEvent) {
+        when (loginEvent) {
             is LoginEvent.EmailChanged -> {
                 updateEmail(loginEvent.email)
             }
+
             is LoginEvent.EmailDeleted -> {
                 deleteEmail()
             }
+
             is LoginEvent.PasswordChanged -> {
                 updatePassword(loginEvent.password)
+            }
+
+            is LoginEvent.Submit -> {
+                submitLogIn()
             }
         }
     }
 
-    private fun updateEmail(email: String){
+    private fun updateEmail(email: String) {
         _userLogInState.value = _userLogInState.value.copy(
             email = email
         )
     }
 
-    private fun deleteEmail(){
+    private fun deleteEmail() {
         _userLogInState.value = _userLogInState.value.copy(
             email = ""
         )
     }
 
-    private fun updatePassword(password: String){
+    private fun updatePassword(password: String) {
         _userLogInState.value = _userLogInState.value.copy(
             password = password
         )
     }
 
-    private fun submitLogIn(){
+    private fun submitLogIn() {
         val emailValidation = validateLogInForms.validateEmail(userLogInState.value.email)
         val passwordValidation = validateLogInForms.validatePassword(userLogInState.value.password)
 
@@ -62,7 +85,7 @@ class LogInCredentialsViewModel( private val validateLogInForms: ValidateLogInFo
             passwordValidation,
         ).any { !it.positiveResult }
 
-        if(hasError){
+        if (hasError) {
             _userLogInState.value = _userLogInState.value.copy(
                 emailErrorMsg = emailValidation.errorMessage,
                 passwordErrorMsg = passwordValidation.errorMessage,
@@ -70,7 +93,38 @@ class LogInCredentialsViewModel( private val validateLogInForms: ValidateLogInFo
             return
         }
         viewModelScope.launch {
-            validationEventChannel.send(ValidationState.Success)
+            try {
+                val loginResponse = AuthService.loginUser(
+                    _userLogInState.value.email,
+                    _userLogInState.value.password
+                )
+                if (loginResponse.status.isSuccess()) {
+                    val jsonObject =
+                        Gson().fromJson(loginResponse.bodyAsText(), JsonObject::class.java)
+                    val token = jsonObject.get("jwt").asString
+                    if (token.isNotEmpty()) {
+                        val userId = jsonObject.getAsJsonObject("user").get("id").asString
+
+                        sharedPreferences.edit().apply {
+                            putString("userId", userId)
+                            apply()
+                        }
+
+                        val encryptedSharedPreferences = EncryptedPreferencesManager.getEncryptedPreferences()
+                        encryptedSharedPreferences.edit().apply {
+                            putString("email", _userLogInState.value.email)
+                            putString("password", _userLogInState.value.password)
+                            apply()
+                        }
+
+                        ApiService.setToken(token)
+                        validationEventChannel.send(ValidationState.Success)
+                    }
+                }
+            } catch (e: Exception) {
+                //TODO error handling
+            }
+
         }
     }
 }
