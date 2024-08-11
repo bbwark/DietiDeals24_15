4580579942738bc446561a08ede346f1188f50fb
bbwark <devivobruno01@gmail.com>
2024-08-11 20:14:44 +0200

    BidHistory retrieve bidders api call created

--- front/app/src/main/java/com/CioffiDeVivo/dietideals/Views/BidHistoryView.kt
+++ front/app/src/main/java/com/CioffiDeVivo/dietideals/Views/BidHistoryView.kt
@@ -1,7 +1,5 @@
 package com.CioffiDeVivo.dietideals.Views
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,6 +14,7 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,26 +27,33 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.CioffiDeVivo.dietideals.Components.BidHistoryElement
 import com.CioffiDeVivo.dietideals.Components.UserInfoBottomSheet
 import com.CioffiDeVivo.dietideals.domain.DataModels.Bid
-import com.CioffiDeVivo.dietideals.domain.DataModels.ObservedUser
+import com.CioffiDeVivo.dietideals.domain.DataModels.User
 import com.CioffiDeVivo.dietideals.viewmodel.BidHistoryViewModel
 import java.time.ZonedDateTime
 
-@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun BidHistoryView(viewModel: BidHistoryViewModel) {
+fun BidHistoryView(viewModel: BidHistoryViewModel, navController: NavHostController) {
     var showDetails by remember { mutableStateOf(false) }
     var acceptOffer by remember { mutableStateOf(false) }
     var userInfo by remember { mutableStateOf(false) }
 
     var bidderName by remember { mutableStateOf("") }
     var selectedBid by remember { mutableStateOf(Bid("", 0f, "", ZonedDateTime.now())) }
-    var selectedUser by remember { mutableStateOf(ObservedUser("", "")) }
+    var selectedUser by remember { mutableStateOf(User("", "")) }
 
     val auctionState by viewModel.auctionState.collectAsState()
     val bidState by viewModel.bidState.collectAsState()
+
+    val bidders = viewModel.auctionBidders.collectAsState().value
+
+    LaunchedEffect(Unit) {
+        viewModel.fetchAuctionBidders()
+    }
 
     Box {
         LazyColumn(
@@ -55,22 +61,22 @@ fun BidHistoryView(viewModel: BidHistoryViewModel) {
                 .fillMaxSize()
                 .padding(horizontal = 12.dp),
             content = {
-                itemsIndexed(viewModel.selectedAuction.bids) { index, item ->
+                itemsIndexed(auctionState.bids) { index, bid ->
                     if (index == 0) {
                         Spacer(modifier = Modifier.height(10.dp))
                     }
-                    viewModel.selectedAuctionBidders.find { it.id == item.userId }?.let {
+                    bidders.find { it.id == bid.userId }?.let {
                         BidHistoryElement(
                             bidderName = it.name,
-                            bidValue = item.value,
+                            bidValue = bid.value,
                             onShowDetails = {
                                 bidderName = it.name
-                                selectedBid = item
+                                selectedBid = bid
                                 showDetails = true
                             },
                             onAcceptOffer = {
                                 bidderName = it.name
-                                selectedBid = item
+                                selectedBid = bid
                                 acceptOffer = true
                             },
                             onUserInfo = {
@@ -83,13 +89,13 @@ fun BidHistoryView(viewModel: BidHistoryViewModel) {
                 }
             })
         if (showDetails) {
-            showDetailsDialog(
+            ShowDetailsDialog(
                 selectedBid = selectedBid,
                 bidderName = bidderName,
                 onDismissRequest = { showDetails = false })
         }
         if (acceptOffer) {
-            acceptOfferDialog(
+            AcceptOfferDialog(
                 selectedBid = selectedBid,
                 bidderName = bidderName,
                 onDismissRequest = { acceptOffer = false },
@@ -101,16 +107,15 @@ fun BidHistoryView(viewModel: BidHistoryViewModel) {
         }
         if (userInfo) {
             UserInfoBottomSheet(
-                observedUser = selectedUser,
+                user = selectedUser,
                 onDismissRequest = { userInfo = false }
             )
         }
     }
 }
 
-@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun showDetailsDialog(
+fun ShowDetailsDialog(
     selectedBid: Bid,
     bidderName: String,
     onDismissRequest: () -> Unit
@@ -138,9 +143,8 @@ fun showDetailsDialog(
     )
 }
 
-@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun acceptOfferDialog(
+fun AcceptOfferDialog(
     selectedBid: Bid,
     bidderName: String,
     onDismissRequest: () -> Unit,
@@ -177,9 +181,8 @@ fun acceptOfferDialog(
     )
 }
 
-@RequiresApi(Build.VERSION_CODES.O)
 @Preview(showBackground = true)
 @Composable
 fun BidHistoryViewPreview() {
-    BidHistoryView(viewModel = BidHistoryViewModel())
+    BidHistoryView(viewModel = BidHistoryViewModel(), navController = rememberNavController())
 }
