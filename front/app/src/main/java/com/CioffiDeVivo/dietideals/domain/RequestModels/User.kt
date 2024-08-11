2752aaa58b44f294c2d9e657a7e7117f15a3fb27
bbwark <devivobruno01@gmail.com>
2024-08-11 02:19:08 +0200

    User Request model changed to conform fixed backend user response

--- front/app/src/main/java/com/CioffiDeVivo/dietideals/domain/RequestModels/User.kt
+++ front/app/src/main/java/com/CioffiDeVivo/dietideals/domain/RequestModels/User.kt
@@ -10,13 +10,14 @@ data class User (
     @SerializedName("email"                    ) var email                    : String?                = null,
     @SerializedName("password"                 ) var password                 : String?                = null,
     @SerializedName("isSeller"                 ) var isSeller                 : Boolean?               = null,
-    @SerializedName("favouriteAuctionEntities" ) var favouriteAuctionEntities : ArrayList<Auction>     = arrayListOf(),
+    @SerializedName("ownedAuctions"            ) var ownedAuctions            : ArrayList<Auction>     = arrayListOf(),
+    @SerializedName("favouriteAuctions"        ) var favouriteAuctions        : ArrayList<Auction>     = arrayListOf(),
     @SerializedName("bio"                      ) var bio                      : String?                = null,
     @SerializedName("address"                  ) var address                  : String?                = null,
     @SerializedName("zipcode"                  ) var zipcode                  : String?                = null,
     @SerializedName("country"                  ) var country                  : String?                = null,
     @SerializedName("phoneNumber"              ) var phoneNumber              : String?                = null,
     @SerializedName("creditCards"              ) var creditCards              : ArrayList<CreditCard>  = arrayListOf(),
-    @SerializedName("authorities"              ) var authorities              : ArrayList<Authority>   = arrayListOf()
+    @SerializedName("authorities"              ) var authorities              : ArrayList<Authority> = arrayListOf()
 
 )
