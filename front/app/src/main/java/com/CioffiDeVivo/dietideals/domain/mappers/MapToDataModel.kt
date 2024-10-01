c926123ec18f05fbf23ff766ffa952ca729c9dbd
bbwark <devivobruno01@gmail.com>
2024-10-01 18:04:52 +0200

    Decoder and Encoder to map correctly among models fixed

--- front/app/src/main/java/com/CioffiDeVivo/dietideals/domain/mappers/MapToDataModel.kt
+++ front/app/src/main/java/com/CioffiDeVivo/dietideals/domain/mappers/MapToDataModel.kt
@@ -54,7 +54,7 @@ fun Item.toDataModel(): com.CioffiDeVivo.dietideals.domain.models.Item {
     return com.CioffiDeVivo.dietideals.domain.models.Item(
         id = this.id ?: "",
         name = this.name ?: "",
-        imagesUri = listOf(Uri.parse(this.imageUrl?:""))
+        imagesUri = this.imageUrl?.split(" ") ?: emptyList()
     )
 }
 
