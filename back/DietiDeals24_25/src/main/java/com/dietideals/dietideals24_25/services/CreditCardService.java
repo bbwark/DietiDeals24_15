cb31ff4e8931455421cca9f1a6a4f20a0d614617
bbwark <devivobruno01@gmail.com>
2024-11-18 16:05:16 +0100

    Bid, Card, Item service new method to retrieve from db

--- back/DietiDeals24_25/src/main/java/com/dietideals/dietideals24_25/services/CreditCardService.java
+++ back/DietiDeals24_25/src/main/java/com/dietideals/dietideals24_25/services/CreditCardService.java
@@ -3,6 +3,7 @@ package com.dietideals.dietideals24_25.services;
 import com.dietideals.dietideals24_25.domain.entities.CreditCardEntity;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
@@ -12,4 +13,5 @@ public interface CreditCardService {
     CreditCardEntity save(CreditCardEntity creditCardEntity);
     void delete (String creditCardNumber);
     List<CreditCardEntity> findByUserId(UUID userId);
+    Optional<CreditCardEntity> findByCardNumber(String cardNumber);
 }
