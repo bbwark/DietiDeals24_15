99f7658532d484b0aa4cef7a13b87af35cdd1813
bbwark <devivobruno01@gmail.com>
2024-11-18 21:06:54 +0100

    Service tag removed from services interfaces

--- back/DietiDeals24_25/src/main/java/com/dietideals/dietideals24_25/services/ItemService.java
+++ back/DietiDeals24_25/src/main/java/com/dietideals/dietideals24_25/services/ItemService.java
@@ -5,9 +5,6 @@ import com.dietideals.dietideals24_25.domain.entities.ItemEntity;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public interface ItemService {
     ItemEntity save(ItemEntity itemEntity);
 
--- back/DietiDeals24_25/src/main/java/com/dietideals/dietideals24_25/services/RoleService.java
+++ back/DietiDeals24_25/src/main/java/com/dietideals/dietideals24_25/services/RoleService.java
@@ -2,11 +2,8 @@ package com.dietideals.dietideals24_25.services;
 
 import java.util.Optional;
 
-import org.springframework.stereotype.Service;
-
 import com.dietideals.dietideals24_25.domain.entities.RoleEntity;
 
-@Service
 public interface RoleService {
     Optional<RoleEntity> findByAuthority(String authority);
 }
