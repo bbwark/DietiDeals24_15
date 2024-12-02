aa6a334bb982ada15c49265b394c2f702a4cc31b
bbwark <devivobruno01@gmail.com>
2024-12-02 17:14:46 +0100

    Custom auction mapping implemented in user in order to show item urls

--- back/DietiDeals24_25/src/main/java/com/dietideals/dietideals24_25/mappers/impl/UserMapperImpl.java
+++ back/DietiDeals24_25/src/main/java/com/dietideals/dietideals24_25/mappers/impl/UserMapperImpl.java
@@ -3,6 +3,7 @@ package com.dietideals.dietideals24_25.mappers.impl;
 import com.dietideals.dietideals24_25.domain.dto.UserDto;
 import com.dietideals.dietideals24_25.domain.entities.UserEntity;
 import com.dietideals.dietideals24_25.mappers.Mapper;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
@@ -10,14 +11,27 @@ import org.springframework.stereotype.Component;
 public class UserMapperImpl implements Mapper<UserEntity, UserDto> {
 
     private ModelMapper modelMapper;
+    private AuctionMapperImpl auctionMapper;
 
-    public UserMapperImpl(ModelMapper modelMapper) {
+    public UserMapperImpl(ModelMapper modelMapper, AuctionMapperImpl auctionMapper) {
         this.modelMapper = modelMapper;
+        this.auctionMapper = auctionMapper;
     }
 
     @Override
     public UserDto mapTo(UserEntity userEntity) {
-        return modelMapper.map(userEntity, UserDto.class);
+        UserDto userDto = modelMapper.map(userEntity, UserDto.class);
+        if (userEntity.getOwnedAuctions() != null) {
+            userDto.setOwnedAuctions(
+                    userEntity.getOwnedAuctions().stream()
+                            .map(auctionMapper::mapTo).toList());
+        }
+        if (userEntity.getFavouriteAuctions() != null) {
+            userDto.setFavouriteAuctions(
+                    userEntity.getFavouriteAuctions().stream()
+                            .map(auctionMapper::mapTo).toList());
+        }
+        return userDto;
     }
 
     @Override
